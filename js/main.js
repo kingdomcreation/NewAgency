685f71b5a9bd698d9efe59963c96f4bf8d80140d
Kingdom Creation <kingdomcreation@gmail.com>
2019-10-04 22:34:00 -0400

    Add scroll to top anchor

--- js/main.js
+++ js/main.js
@@ -59,4 +59,14 @@ jQuery("#button").on("click", button_click);
       $('.navbar').removeClass('d-none');
     })
   
+    // Scroll to top button appear
+    $(document).scroll(function() {
+      var scrollDistance = $(this).scrollTop();
+      if (scrollDistance > 640) {
+        $('.scroll-to-top').fadeIn();
+      } else {
+        $('.scroll-to-top').fadeOut();
+      }
+    });
+  
   })(jQuery); // End of use strict  
